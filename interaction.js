9822e09f287d2169686e88af4fd8047ec960c09d
Rysopanha-Lim-developer <sopanhalimry@gmail.com>
2026-06-21 08:51:08 +0700

    finished responsive screen for max-width:600px and for min-width:600px and max-width:768px on nav bar, contact, minigame page. (#7)

--- interaction.js
+++ interaction.js
@@ -1,93 +1,37 @@
-//list toggle
-const toggler = document.getElementById("toggler");
-const miniList = [...document.querySelectorAll("#mini-list")]
+//Resoponsive interaction
 
-toggler.addEventListener("click", ()=>{
-    toggler.classList.toggle("spin")
-    miniList.forEach(element => {
-        element.classList.toggle("shown")
-    });
-})
+document.addEventListener('DOMContentLoaded', () => {
 
-//guessing game
-const userInputNums = document.getElementById("nums-input");
-const userSubmit = document.getElementById("nums-input-submit");
-const userReset = document.getElementById("nums-reset-submit");
-const guessingResult = document.getElementById("guessing-result");
-const allowedAttemp = document.getElementById("allowed-attempt")
-let randomNums =  Math.floor((Math.random()*10)+1);
-let maxattempts = 3;
-let usedattempts= 1;
+    //list toggle
+    const toggler = document.getElementById("toggler");
+    const miniList = [...document.querySelectorAll("#mini-list")]
 
-userSubmit.addEventListener("click", ()=>{
-    RandomNums(userInputNums.value)
-})
-userReset.addEventListener("click", ()=>{
-    maxattempts = 3;
-    randomNums =  Math.floor((Math.random()*10)+1);
-    allowedAttemp.textContent = `You have 3 chances`
-    userInputNums.value = ''
-    userSubmit.style.cursor = "pointer"
-    guessingResult.textContent = `Result`
-})
+    toggler.addEventListener("click", ()=>{
+        toggler.classList.toggle("spin")
+        miniList.forEach(element => {
+            element.classList.toggle("shown")
+        });
+    })
 
-function RandomNums(userInput){
+    
+    const navbarDropper = document.getElementById("nav-dropper")
+    const navbarRetracker = document.getElementById("nav-retract")
+    const navbar = document.getElementById("nav-bar")
+    const header = document.getElementById("header")
 
-    if(maxattempts >= 1){
-        if(Number(userInput) === randomNums){
-            guessingResult.textContent = `You win check your reward on the list`
-            console.log("win")
-        }
-        else{
-            maxattempts -= usedattempts
-            allowedAttemp.textContent = `You have ${maxattempts} chances left`
-            console.log("lose")
-            if(maxattempts === 0){
-                guessingResult.textContent = `You Lost it is ${randomNums}`
-                allowedAttemp.textContent = `You have ${maxattempts} chances left`
-                userSubmit.style.cursor = "not-allowed"
-            }
-        }
-    }
+    navbarDropper.addEventListener("click", ()=>{
+        header.classList.add("dropdown")
+        navbar.removeAttribute('inert')
+        navbar.classList.add('show')
+        navbarDropper.classList.add('close')
+        navbarRetracker.classList.add('open')
+    })
+    navbarRetracker.addEventListener("click", ()=>{
+        header.classList.remove("dropdown")
+        navbar.setAttribute('inert', '')
+        navbar.classList.remove('show')
+        navbarDropper.classList.remove('close')
+        navbarRetracker.classList.remove('open')
+    })
 
-}
-
-
-//dice game
-const startRollingBtn = document.getElementById("start-rolling")
-const diceContainer = document.getElementById("dice-container")
-const diceResult = document.getElementById("dice-result")
-
-startRollingBtn.addEventListener("click", DiceRoller) 
-
-function DiceRoller(){
-    let resultGen = Math.floor(Math.random() * 6) + 1;
-
-    switch(resultGen){
-        case 1:
-            diceContainer.style.backgroundImage = "url('/web-image/dice-six-faces-one.png')"
-            diceResult.textContent = `you dice landed on ${resultGen}`
-            break;
-        case 2:
-            diceContainer.style.backgroundImage = "url('/web-image/dice-six-faces-two.png')"
-            diceResult.textContent = `you dice landed on ${resultGen}`
-            break;
-        case 3:
-            diceContainer.style.backgroundImage = "url('/web-image/dice-six-faces-three.png')"
-            diceResult.textContent = `you dice landed on ${resultGen}`
-            break;
-        case 4:
-            diceContainer.style.backgroundImage = "url('/web-image/dice-six-faces-four.png')"
-            diceResult.textContent = `you dice landed on ${resultGen}`
-            break;
-        case 5:
-            diceContainer.style.backgroundImage = "url('/web-image/dice-six-faces-five.png')"
-            diceResult.textContent = `you dice landed on ${resultGen}`
-            break;
-        case 6:
-            diceContainer.style.backgroundImage = "url('/web-image/dice-six-faces-six.png')"
-            diceResult.textContent = `you dice landed on ${resultGen}`
-            break;
-    }
-}
-
+});
